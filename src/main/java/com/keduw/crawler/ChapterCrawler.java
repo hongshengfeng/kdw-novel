3dbc761d28648d10f9b6b4c5c00146eec156a85c
Mr.Hong <1083178465@qq.com>
2018-10-07 20:32:10 +0800

    修改爬虫工具类

--- src/main/java/com/keduw/crawler/ChapterCrawler.java
+++ src/main/java/com/keduw/crawler/ChapterCrawler.java
@@ -5,7 +5,10 @@ import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import com.keduw.model.Chapter;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.concurrent.BlockingQueue;
 
 /**
  * 小说章节内容爬取
@@ -13,15 +16,17 @@ import org.jsoup.select.Elements;
 public class ChapterCrawler extends BreadthCrawler {
 
     private Chapter chapter;
-    private String REGEX = "https://www.biquge5.com/[0-9]+_[0-9]+/[0-9]+.html"; // 采集规则
+    private String REGEX = "https://www.biquge5.com/[0-9]+_[0-9]+/[0-9]+[_[0-9]+]?.html"; // 采集规则
+    private BlockingQueue<Chapter> chapterQueue = null; // 有多页面则替换链接后存储到该队列
 
-    public ChapterCrawler(String crawlPath, boolean autoParse, Chapter curr) {
+    public ChapterCrawler(String crawlPath, boolean autoParse, Chapter curr, BlockingQueue<Chapter> queue) {
         super(crawlPath, autoParse);
-        this.chapter = curr;
-        this.addSeed(chapter.getChapterUrl());
+        this.addSeed(curr.getChapterUrl());
         this.addRegex("-.*\\.(jpg|png|gif).*");
         this.setThreads(1);
         this.setResumable(false); //停止后下次继续爬取
+        this.chapter = curr;
+        this.chapterQueue = queue;
     }
 
     @Override
@@ -30,7 +35,28 @@ public class ChapterCrawler extends BreadthCrawler {
             Document document = page.doc();
             Elements contents = document.select("div[id=content]");
             String content = contents.get(0).html();
+            //同一章节分多页则拼接内容
+            String preContent = chapter.getContent();
+            if(preContent != null){
+                content = preContent + content;
+            }
             chapter.setContent(content);
+            //存在下一页则表示该章节内容还没完，生成一个爬虫爬取下一页的内容
+            Elements nextPage = document.select("div[class=bottem1]");
+            Element element = nextPage.get(0).getElementsByTag("a").get(4);
+            String nextContent = element.text();
+            if(nextContent.equals("下一页")){
+                // 有下一页则进行内容替换和存储到队列里
+                String nextUrl = element.attr("href");
+                String preUrl = chapter.getChapterUrl();
+                nextUrl = preUrl.substring(0, preUrl.lastIndexOf("/") + 1) + nextUrl;
+                chapter.setChapterUrl(nextUrl);
+                try{
+                    chapterQueue.put(chapter);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/main/java/com/keduw/crawler/NovelSchedule.java
+++ src/main/java/com/keduw/crawler/NovelSchedule.java
@@ -6,12 +6,14 @@ import com.keduw.service.ChapterService;
 import com.keduw.service.NovelService;
 import com.keduw.util.ApplicationUtil;
 import com.keduw.util.JsonUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 小说爬虫
@@ -53,23 +55,34 @@ public class NovelSchedule {
     }
 
     //每天1点爬取章节内容
-    @Scheduled(cron = "0 0 10 * * ? ")
+    @Scheduled(cron = "0 22 20 * * ? ")
     public void infoCollect() throws Exception{
         //获取总章节数
         ChapterService chapterService = (ChapterService) ApplicationUtil.getBean("chapterService");
         int counts = chapterService.getInfoCounts();
         int times = counts / 1000;
         times = counts % 1000 == 0 ? times : times + 1;
-        System.out.println("总批次：" + times);
         List<Chapter> chapterList = new ArrayList<>();
         for(int i = 1; i <= times; i++){
             chapterList = chapterService.getChapterList(i, 1000);
+            // 阻塞队列用于存储一个章节内有多个页面的章节
+            BlockingQueue<Chapter> chapterQueue = new LinkedBlockingQueue<Chapter>(10000 * 10);
             for(Chapter chapter : chapterList){
-                ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter);
-                crawler.start(2);
+                ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
+                crawler.start(1);
                 while (crawler.isResumable()){
                 }
             }
+            // 爬取下一页
+            while (chapterQueue.size() > 0){
+                Chapter chapter = chapterQueue.poll(100, TimeUnit.MILLISECONDS);
+                ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
+                crawler.start(1);
+                while (crawler.isResumable()){
+                }
+            }
+            // 更新章节内容
+            chapterService.updateChapter(chapterList);
             System.out.println("已更新批次：" + i);
         }
         System.out.println("更新结束");
