54285719cd1d569523faedaffcbfe01ea2318c26
Mr.Hong <1083178465@qq.com>
2018-10-19 22:50:04 +0800

    修改小说爬虫生产和消费的启动顺序

--- src/main/java/com/keduw/crawler/NovelCrawler.java
+++ src/main/java/com/keduw/crawler/NovelCrawler.java
@@ -7,6 +7,7 @@ import com.keduw.model.Novel;
 import com.keduw.model.NovelColl;
 import com.keduw.util.BaseUtil;
 import com.keduw.util.CateUtil;
+import com.keduw.util.JsonUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -41,51 +42,52 @@ public class NovelCrawler extends BreadthCrawler {
     @Override
     public void visit(Page page, CrawlDatums crawlDatums) {
         if(page.matchUrl(REGEX)) {
-            Document document = page.doc();
-            Elements baseInfo = document.select("div[id=info]");
-            Novel novel = new Novel();
-            if(baseInfo != null && !baseInfo.isEmpty()){
-                Element info = baseInfo.get(0);
-                String title = info.getElementsByTag("h1").get(0).text(); //标题
-                String author = info.getElementsByTag("p").get(0).text(); //作者
-                String status = info.getElementsByTag("p").get(1).text(); //更新状态
-                String lastTime = info.getElementsByTag("p").get(2).text(); //最后更新时间
-                novel.setNovelName(title);
-                novel.setAuthor(BaseUtil.tirmStr(author));
-                novel.setStatus(BaseUtil.tirmStr(status));
-                novel.setLastTime(BaseUtil.tirmStr(lastTime));
-                novel.setNovelUrl(page.url());
-            }
-            //类别
-            Elements conTop = document.select("div[class=con_top]");
-            if(!conTop.isEmpty()){
-                String category = conTop.get(0).getElementsByTag("a").get(4).text();
-                novel.setCategoryId(CateUtil.getId(category, 0));
-            }
-            //简介
-            Elements intro = document.select("div[id=intro]");
-            if(!intro.isEmpty()){
-                String brief = intro.get(0).getElementsByTag("p").get(0).text();
-                novel.setBrief(brief);
-            }
-            //章节
-            Elements chapter = document.select("ul[class=_chapter] li a");
-            List<Chapter> chapterList = new ArrayList<>();
-            for (Element element:chapter) {
-                Chapter info =  new Chapter();
-                String url =  element.attr("href");
-                String content = element.text();
-                info.setNovelId(novel.getNovelId());
-                info.setChapter(content);
-                info.setChapterUrl(BaseUtil.urlTrim(url));
-                chapterList.add(info);
-            }
-            novel.setChapterSize(chapter.size());
-            //将爬取信息存储到队列中
-            NovelColl novelColl = new NovelColl();
-            novelColl.setNovel(novel);
-            novelColl.setChapters(chapterList);
             try {
+                Document document = page.doc();
+                Elements baseInfo = document.select("div[id=info]");
+                Novel novel = new Novel();
+                if(baseInfo != null && !baseInfo.isEmpty()){
+                    Element info = baseInfo.get(0);
+                    String title = info.getElementsByTag("h1").get(0).text(); //标题
+                    String author = info.getElementsByTag("p").get(0).text(); //作者
+                    String status = info.getElementsByTag("p").get(1).text(); //更新状态
+                    String lastTime = info.getElementsByTag("p").get(2).text(); //最后更新时间
+                    novel.setNovelName(title);
+                    novel.setAuthor(BaseUtil.tirmStr(author));
+                    novel.setStatus(BaseUtil.tirmStr(status));
+                    novel.setLastTime(BaseUtil.tirmStr(lastTime));
+                    novel.setNovelUrl(page.url());
+                }
+                System.out.println(JsonUtils.objectToJson(novel));
+                //类别
+                Elements conTop = document.select("div[class=con_top]");
+                if(!conTop.isEmpty()){
+                    String category = conTop.get(0).getElementsByTag("a").get(4).text();
+                    novel.setCategoryId(CateUtil.getId(category, 0));
+                }
+                //简介
+                Elements intro = document.select("div[id=intro]");
+                if(!intro.isEmpty()){
+                    String brief = intro.get(0).getElementsByTag("p").get(0).text();
+                    novel.setBrief(brief);
+                }
+                //章节
+                Elements chapter = document.select("ul[class=_chapter] li a");
+                List<Chapter> chapterList = new ArrayList<>();
+                for (Element element:chapter) {
+                    Chapter info =  new Chapter();
+                    String url =  element.attr("href");
+                    String content = element.text();
+                    info.setNovelId(novel.getNovelId());
+                    info.setChapter(content);
+                    info.setChapterUrl(BaseUtil.urlTrim(url));
+                    chapterList.add(info);
+                }
+                novel.setChapterSize(chapter.size());
+                //将爬取信息存储到队列中
+                NovelColl novelColl = new NovelColl();
+                novelColl.setNovel(novel);
+                novelColl.setChapters(chapterList);
                 novelQueue.put(novelColl);
             }catch (Exception e){
                 e.printStackTrace();
--- src/main/java/com/keduw/crawler/NovelSchedule.java
+++ src/main/java/com/keduw/crawler/NovelSchedule.java
@@ -6,6 +6,7 @@ import com.keduw.service.ChapterService;
 import com.keduw.service.NovelService;
 import com.keduw.util.ApplicationUtil;
 import com.keduw.util.JsonUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,39 +16,43 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-/**
- * 小说爬虫
- */
+//小说爬虫
 @Component
 public class NovelSchedule {
 
+    private boolean isOpen = true;
+
     //每周六的0点启动小说爬虫，爬取是否有新小说
-    @Scheduled(cron = "0 0 0 * * SAT")
+    @Scheduled(cron = "0 12 22 * * *")
     public void novelCollect() throws Exception{
-        NovelCrawler crawl = new NovelCrawler("crawl",true);
-        crawl.start(5);
-        NovelInfoThread saveInfo = new NovelInfoThread(crawl.novelQueue);
-        Thread thread = new Thread(saveInfo);
-        thread.start();
+        if(isOpen){
+            NovelCrawler crawl = new NovelCrawler("crawl",true);
+            NovelInfoThread saveInfo = new NovelInfoThread(crawl.novelQueue);
+            Thread thread = new Thread(saveInfo);
+            thread.start();
+            crawl.start(5);
+        }
     }
 
     //每天0点30分检查连载中小说的章节更新情况
     @Scheduled(cron = "0 30 0 * * ? ")
     public void infoCheck() throws Exception{
         //小说总数
-        NovelService novelService = (NovelService) ApplicationUtil.getBean("novelService");
-        int counts = novelService.getNovelCount();
-        int times = counts / 100;
-        times = counts % 100 == 0 ? times : times + 1;
-        // 分批次检查小说的章节
-        List<Novel> novelList = new ArrayList<>();
-        for(int i = 1; i <= times; i++){
-            novelList = novelService.getNovelList(i, 100);
-            for(Novel novel : novelList){
-                if(novel.getStatus().equals("连载中")){
-                    CheckCrawler crawler = new CheckCrawler("crawl",true, novel);
-                    crawler.start(2);
-                    while (crawler.isResumable()){
+        if(isOpen){
+            NovelService novelService = (NovelService) ApplicationUtil.getBean("novelService");
+            int counts = novelService.getNovelCount();
+            int times = counts / 100;
+            times = counts % 100 == 0 ? times : times + 1;
+            // 分批次检查小说的章节
+            List<Novel> novelList = new ArrayList<>();
+            for(int i = 1; i <= times; i++){
+                novelList = novelService.getNovelList(i, 100);
+                for(Novel novel : novelList){
+                    if(novel.getStatus().equals("连载中")){
+                        CheckCrawler crawler = new CheckCrawler("crawl",true, novel);
+                        crawler.start(2);
+                        while (crawler.isResumable()){
+                        }
                     }
                 }
             }
@@ -58,31 +63,33 @@ public class NovelSchedule {
     @Scheduled(cron = "0 51 21 * * ? ")
     public void infoCollect() throws Exception{
         //获取总章节数
-        ChapterService chapterService = (ChapterService) ApplicationUtil.getBean("chapterService");
-        int counts = chapterService.getInfoCounts();
-        int times = counts / 100;
-        times = counts % 100 == 0 ? times : times + 1;
-        List<Chapter> chapterList = new ArrayList<>();
-        for(int i = 1; i <= times; i++){
-            chapterList = chapterService.getChapterList(i, 100);
-            // 阻塞队列用于存储一个章节内有多个页面的章节
-            BlockingQueue<Chapter> chapterQueue = new LinkedBlockingQueue<Chapter>(10000 * 10);
-            for(Chapter chapter : chapterList){
-                ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
-                crawler.start(1);
+        if(isOpen){
+            ChapterService chapterService = (ChapterService) ApplicationUtil.getBean("chapterService");
+            int counts = chapterService.getInfoCounts();
+            int times = counts / 100;
+            times = counts % 100 == 0 ? times : times + 1;
+            List<Chapter> chapterList = new ArrayList<>();
+            for(int i = 1; i <= times; i++){
+                chapterList = chapterService.getChapterList(i, 100);
+                // 阻塞队列用于存储一个章节内有多个页面的章节
+                BlockingQueue<Chapter> chapterQueue = new LinkedBlockingQueue<Chapter>(10000 * 10);
+                for(Chapter chapter : chapterList){
+                    ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
+                    crawler.start(1);
+                }
+                // 爬取下一页
+                while (chapterQueue.size() > 0){
+                    Chapter chapter = chapterQueue.poll(100, TimeUnit.MILLISECONDS);
+                    ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
+                    crawler.start(1);
+                    System.out.println("章节内容：" + JsonUtils.objectToJson(chapter));
+                }
+                // 更新章节内容
+                chapterService.updateChapter(chapterList);
+                System.out.println("已更新批次：" + i);
             }
-            // 爬取下一页
-            while (chapterQueue.size() > 0){
-                Chapter chapter = chapterQueue.poll(100, TimeUnit.MILLISECONDS);
-                ChapterCrawler crawler = new ChapterCrawler("crawl", true, chapter, chapterQueue);
-                crawler.start(1);
-                System.out.println("章节内容：" + JsonUtils.objectToJson(chapter));
-            }
-            // 更新章节内容
-            chapterService.updateChapter(chapterList);
-            System.out.println("已更新批次：" + i);
+            System.out.println("更新结束");
         }
-        System.out.println("更新结束");
     }
 
 }
