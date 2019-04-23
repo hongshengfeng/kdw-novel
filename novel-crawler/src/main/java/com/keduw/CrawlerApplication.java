7b050e7ff33e2974d9ec472487f4010800ec64a6
Mr.Hong <1083178465@qq.com>
2019-04-23 23:07:17 +0800

    开启事务管理

--- novel-crawler/src/main/java/com/keduw/CrawlerApplication.java
+++ novel-crawler/src/main/java/com/keduw/CrawlerApplication.java
@@ -1,14 +1,15 @@
 package com.keduw;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.keduw.dao")
+@EnableTransactionManagement
 public class CrawlerApplication {
 
 	public static void main(String[] args) {
--- novel-portal/src/main/java/com/keduw/NovelApplication.java
+++ novel-portal/src/main/java/com/keduw/NovelApplication.java
@@ -3,13 +3,13 @@ package com.keduw;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.keduw.dao")
+@EnableTransactionManagement
 public class NovelApplication{
 
 
